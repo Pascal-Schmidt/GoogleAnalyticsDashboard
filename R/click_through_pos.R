2b302bf04618e8140e635e184e632f2756c4c655
Pascal Schmidt <pascal@Philippe-LBC-Macbook-Pro.local>
2021-03-21 16:28:09 -0700

    removed hard coded data set

--- R/click_through_pos.R
+++ R/click_through_pos.R
@@ -1,23 +1,23 @@
 click_through_pos <- function(df) {
 
-web_data_c %>%
-  dplyr::group_by(Position = round(position)) %>%
-  dplyr::summarise(`Average CTR` = round(mean(ctr)*100, 2)) %>%
-  dplyr::arrange(Position) %>%
-  .[1:20, ] -> df_test
+  df %>%
+    dplyr::group_by(Position = round(position)) %>%
+    dplyr::summarise(`Average CTR` = round(mean(ctr)*100, 2)) %>%
+    dplyr::arrange(Position) %>%
+    .[1:20, ] -> df_test
 
-plotly::plot_ly(df_test, x = ~Position,
-                text = ~paste0(`Average CTR`, "%"),
-                hoverinfo = 'text',
-                hovertext = ~paste('Average click through rate <br>
+  plotly::plot_ly(df_test, x = ~Position,
+                  text = ~paste0(`Average CTR`, "%"),
+                  hoverinfo = 'text',
+                  hovertext = ~paste('Average click through rate <br>
                                    for position: ', Position,
-                                   'is', `Average CTR`),
-                showlegend = FALSE) %>%
-  add_trace(y = ~`Average CTR`,
-            type = 'scatter',
-            mode = 'lines+markers+text',
-            textposition = "top right",
-            showarrow = TRUE) -> fig
+                                     'is', `Average CTR`),
+                  showlegend = FALSE) %>%
+    add_trace(y = ~`Average CTR`,
+              type = 'scatter',
+              mode = 'lines+markers+text',
+              textposition = "top right",
+              showarrow = TRUE) -> fig
 
   return(fig)
 
